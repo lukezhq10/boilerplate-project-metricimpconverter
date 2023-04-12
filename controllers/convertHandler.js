41d74a07db47ec32534b355e6bb6ed4e75c0f2f5
Luke Zhang <you@example.com>
2023-04-12 12:11:09 -0600

    initial commit

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,41 +1,86 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    // This regex pattern matches any fractional input, decimal or fractional with a fraction slash
+    const fractionPattern = /^(\d+(\.\d+)?|\d*\/\d+|\d+\s\d*\/\d+)(\s*\w*)$/;
+    const match = input.match(fractionPattern);
+    if (!match) {
+      return 1; // If no valid number is found, return 1 by default
+    }
+    // Get the number part of the input and return it
+    return eval(match[1].replace('/', '+')); // Using eval here to handle fractions and decimals correctly
   };
   
   this.getUnit = function(input) {
+    // This regex pattern matches any valid unit input
+    const unitPattern = /gal|lbs|mi|km|l|kg$/i;
+    const match = input.match(unitPattern);
     let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+    if (!match) {
+      return null;
+    } else if (match) { // Get the unit part of the input and return it
+      result = match[0].toLowerCase();
+      if (result === 'l') { // if unit is l, return as uppercase
+        result = 'L';
+      }
+    }
+
     return result;
   };
 
+  // The rest of the functions remain unchanged
+  this.getReturnUnit = function(initUnit) {
+    const unitConversions = {
+      gal: "L",
+      L: "gal",
+      lbs: "kg",
+      kg: "lbs",
+      mi: "km",
+      km: "mi"
+    };
+
+    // Get the corresponding return unit based on the initUnit
+    return unitConversions[initUnit];
+  };
+
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+     // Object to store unit spellings
+     const unitSpellings = {
+      gal: "gallons",
+      L: "liters",
+      lbs: "pounds",
+      kg: "kilograms",
+      mi: "miles",
+      km: "kilometers"
+    };
+
+    // Get the corresponding unit spelling for the given unit
+    return unitSpellings[unit];
   };
   
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    // Object to store conversion factors
+    const unitFactors = {
+      gal: 3.78541,
+      L: 1/3.78541,
+      lbs: 0.453592,
+      kg: 1/0.453592,
+      mi: 1.60934,
+      km: 1/1.60934
+    };
+    // Convert the initNum to the corresponding output value using the unitFactors
+    const result = initNum * unitFactors[initUnit];
+    // Round the result to 5 decimal places
+    return parseFloat(result.toFixed(5));
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
+    // Get the spelled-out input unit and output unit using the spellOutUnit function
+    const spelledInitUnit = this.spellOutUnit(initUnit);
+    const spelledReturnUnit = this.spellOutUnit(returnUnit);
+
+    // Construct the output string based on the input and output values
+    let result = `${initNum} ${spelledInitUnit} converts to ${returnNum} ${spelledReturnUnit}`;
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -7,4 +7,25 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
+  app.route('/api/convert')
+    .get((req, res) => {
+      // convert the req, res with convertHandler.getString and the res.json
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var result = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+
+      
+      return res.json({
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: result
+      });
+      
+    });
+
 };
